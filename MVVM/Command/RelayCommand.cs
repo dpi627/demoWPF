aec235702ade8baecdb0820ff62ebb7ca2dacdef
APAC\Brian_Li <brian.li@sgs.com>
2024-02-23 16:22:22 +0800

    fet: RelayCommand 改為支援參數，型態 object

--- MVVM/Command/RelayCommand.cs
+++ MVVM/Command/RelayCommand.cs
@@ -10,22 +10,22 @@ namespace MVVM.Command
 
     public class RelayCommand : ICommand
     {
-        private readonly Action _execute;
-        private readonly Func<bool>? _canExecute;
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool>? _canExecute;
 
-        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
 
         public void Execute(object? parameter)
         {
             if (CanExecute(parameter))
             {
-                _execute();
+                _execute(parameter);
             }
         }
 
